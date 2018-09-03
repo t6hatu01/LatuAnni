0058a35af3261d8c14178fc67dbaa4d37bc289a0
Niko <c6koni00@students.oamk.fi>
2018-09-03 12:37:35 +0300

    battleship turns

---
+++ app/src/main/java/com/example/diglet/turns/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.diglet.turns;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
